0d7730d65cc844aeaa0b660303b68b68db2c0821
Marlon-vc <mvegach2013@gmail.com>
2019-04-18 17:53:50 -0600

    Agregadas estructuras de datos

---
+++ src/Structures/LinkedList.java
@@ -0,0 +1,36 @@
+package Structures;
+
+public class LinkedList<T> {
+    private int size;
+    private Node<T> head;
+    private Node<T> tail;
+
+    public LinkedList() {
+        this.size = 0;
+        this.head = this.tail = null;
+    }
+
+    public void add(T element) {
+
+    }
+
+    public void addFirst(T element) {
+
+    }
+
+    public void addLast(T element) {
+
+    }
+
+    public void remove(T element) {
+
+    }
+
+    public void remove(int index) {
+
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
