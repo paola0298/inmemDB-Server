58ad69dc46fb09d02373877b0f12554d8479e382
paola0298 <pvillegasch@gmail.com>
2019-05-09 00:05:40 -0600

    Pruebas

--- src/HashTableTest.java
+++ src/HashTableTest.java
@@ -1,3 +1,4 @@
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.Hashtable;
@@ -6,18 +7,83 @@ public class HashTableTest {
 
 
     public static void main(String[] args) {
-        JSONObject json = new JSONObject();
 
-        Hashtable<String, JSONObject> schemes = new Hashtable<>();
-
-        schemes.put("Prueba", json);
-
-        JSONObject hola = schemes.get("hola");
-
-        System.out.println(hola);
+        Hashtable<String, Hashtable<String, JSONObject>> collections = new Hashtable<>();
 
 
+        Hashtable<String, JSONObject> registros = new Hashtable<>();
 
+        JSONObject registro1 = new JSONObject();
+        JSONArray array = new JSONArray();
+        array.put("nombre");
+        array.put("cedula");
+        array.put("Carro");
+        registro1.put("attr_name", array);
+
+        JSONArray arraySize = new JSONArray();
+        arraySize.put("123");
+        arraySize.put("123");
+        arraySize.put("Join");
+        registro1.put("attr_size", arraySize);
+
+        registros.put("cedula1", registro1);
+
+
+
+
+
+        JSONObject registro2 = new JSONObject();
+        registro2.put("nombre", "nombre2...");
+        registro2.put("cedula", "cedula2...");
+        registros.put("cedula2...", registro2);
+
+        JSONArray arraySize2 = new JSONArray();
+        arraySize2.put("123");
+        arraySize2.put("123");
+        registro2.put("attr_size", arraySize2);
+
+
+        JSONObject registro3 = new JSONObject();
+        registro3.put("nombre", "nombre3...");
+        registro3.put("cedula", "cedula3...");
+
+        JSONArray arraySize3 = new JSONArray();
+        arraySize3.put("123");
+        arraySize3.put("123");
+        registro3.put("attr_size", arraySize3);
+
+
+
+        registros.put("cedula3...", registro3);
+
+
+
+
+
+        collections.put("esquema1", registros);
+
+
+
+        // esquema 1
+
+
+        Hashtable<String, JSONObject> collec = collections.get("esquema1");
+
+
+        if (collec != null){
+
+            for (JSONObject item : collec.values()){
+
+                System.out.println(item);
+                JSONArray sizeAttr = item.getJSONArray("attr_size");
+                sizeAttr.forEach(value -> {
+                    if (value.equals("Join")){
+                        System.out.println(value);
+                    }
+                });
+            }
+            System.out.println("No hay joins en el esquema");
+        }
 
     }
 
